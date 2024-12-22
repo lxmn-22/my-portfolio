472e34002e79b7eacb29235b30912d5f4d898b1e
Laxman Singh Koranga <korangalaxman03@gmail.com>
2024-12-22 06:35:12 +0530

    [my-portfolio]: Added Web3Forms to send contact form data on Email.

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -7,6 +7,32 @@ import github_icon from '../../assets/github-fill.svg';
 import x_icon from '../../assets/twitter-x-fill.svg';
 
 const Contact = () => {
+
+    const [result, setResult] = React.useState("");
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        setResult("Sending...");
+        const formData = new FormData(event.target);
+
+        formData.append("access_key", import.meta.env.VITE_FORMS_ACCESS_KEY);
+
+        const response = await fetch("https://api.web3forms.com/submit", {
+            method: "POST",
+            body: formData
+        });
+
+        const data = await response.json();
+
+        if (data.success) {
+            setResult("Form Submitted Successfully.");
+            event.target.reset();
+        } else {
+            console.log("Error", data);
+            setResult(data.message);
+        }
+    };
+
   return (
     <div id="contact" className='flex flex-col items-center justify-center gap-12 mx-20 my-36'>
         <div className='relative'>
@@ -20,39 +46,40 @@ const Contact = () => {
 
                 <div className='flex flex-col gap-7 text-[#D8D8D8] text-xl'>
                     <div className='flex items-center gap-5'>
-                        <img className='transition ease-in-out delay-10 hover:-translate-y-1 hover:scale-150 duration-300' src={mail_icon} alt="mail-icon" /><p>laxmankoranga03@gmail.com</p>
+                        <img className='transition ease-in-out delay-10 hover:-translate-y-1 hover:scale-150 duration-300' title='Email' src={mail_icon} alt="mail-icon" /><p>laxmankoranga03@gmail.com</p>
                     </div>
                     <div className='flex items-center gap-5'>
-                        <img className='transition ease-in-out delay-10 hover:-translate-y-1 hover:scale-150 duration-300' src={call_icon} alt="call-icon" /><p>+91 9410996867</p>
+                        <img className='transition ease-in-out delay-10 hover:-translate-y-1 hover:scale-150 duration-300' title='Mobile Number' src={call_icon} alt="call-icon" /><p>+91 9410996867</p>
                     </div>
                     <div className='flex items-center gap-5'>
-                        <img className='transition ease-in-out delay-10 hover:-translate-y-1 hover:scale-150 duration-300' src={location_icon} alt="location-icon" /><p>Uttarakhand, India</p>
+                        <img className='transition ease-in-out delay-10 hover:-translate-y-1 hover:scale-150 duration-300' title='Location' src={location_icon} alt="location-icon" /><p>Uttarakhand, India</p>
                     </div>
                     <div className='flex items-center gap-28 text-lg font-medium mb-20'>
                     <div className='flex gap-4 mt-4'>
                         <a href="https://www.linkedin.com/in/laxmankoranga03/" target='_blank' rel="noopener noreferrer">
-                            <img className='w-9 h-8 cursor-pointer transition ease-in-out delay-10 hover:-translate-y-1 hover:scale-150 duration-300' src={linkedIn_icon} alt="linkedIn-icon" />
+                            <img className='w-9 h-8 cursor-pointer transition ease-in-out delay-10 hover:-translate-y-1 hover:scale-150 duration-300' title='LinkedIn' src={linkedIn_icon} alt="linkedIn-icon" />
                         </a>
                         <a href="https://github.com/lxmn-22" target='_blank' rel="noopener noreferrer">
-                            <img className='w-9 h-8 cursor-pointer transition ease-in-out delay-10 hover:-translate-y-1 hover:scale-150 duration-300' src={github_icon} alt="github-icon" />
+                            <img className='w-9 h-8 cursor-pointer transition ease-in-out delay-10 hover:-translate-y-1 hover:scale-150 duration-300' title='Github' src={github_icon} alt="github-icon" />
                         </a>
                         <a href="https://x.com/lxmn22nov" target='_blank' rel="noopener noreferrer">
-                            <img className='w-9 h-8 cursor-pointer transition ease-in-out delay-10 hover:-translate-y-1 hover:scale-150 duration-300' src={x_icon} alt="x_icon" />
+                            <img className='w-9 h-8 cursor-pointer transition ease-in-out delay-10 hover:-translate-y-1 hover:scale-150 duration-300' title='X' src={x_icon} alt="x_icon" />
                         </a>
                     </div>
                     </div>
                 </div>
             </div>
 
-            <form className='flex flex-col items-start gap-7'>
+            <form onSubmit={onSubmit} className='flex flex-col items-start gap-7'>
                 <label className='text-[#D8D8D8] text-xl font-medium' htmlFor="">Name</label>
                 <input className='border-none w-[400px] h-[50px] pl-5 rounded bg-[#32323c] text-[#A0A0A0] text-l' type="text" placeholder='Enter your name' name='name' />
                 <label className='text-[#D8D8D8] text-xl font-medium' htmlFor="">Email</label>
                 <input className='border-none w-[400px] h-[50px] pl-5 rounded bg-[#32323c] text-[#A0A0A0] text-l' type="email" placeholder='Enter your email' name='email' />
                 <label className='text-[#D8D8D8] text-xl font-medium' htmlFor="">Write your message here</label>
                 <textarea className='border-none w-[400px] h-[170px] p-5 rounded bg-[#32323c] text-[#A0A0A0] text-l' name="message" rows="12" placeholder='Enter your message'></textarea>
-                <button className='border-none text-white rounded-3xl bg-primary text-md px-6 py-2 mb-12 cursor-pointer transition ease-in-out delay-10 hover:-translate-y-1 hover:scale-110 duration-300' type=''>Submit</button>
+                <button className='border-none text-white rounded-3xl bg-primary text-md px-6 py-2 mb-12 cursor-pointer transition ease-in-out delay-10 hover:-translate-y-1 hover:scale-110 duration-300' type='submit'>Submit</button>
             </form>
+            {/* <span>{result}</span> */}
         </div>
     </div>
   )
