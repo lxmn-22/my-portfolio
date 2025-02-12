716491f2ae94a9ae1ffa28521cba09a74eb97b86
Laxman Singh Koranga <korangalaxman03@gmail.com>
2025-02-12 20:08:46 +0530

    [my-portfolio] Added & Updated Favicon and NavBar.

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,23 @@
-import React from 'react'
-import Navbar from './components/navbar/Navbar'
-import Hero from './components/hero/Hero'
-import About from './components/about/About'
-import Services from './components/services/Services'
-import Projects from './components/projects/Projects'
-import Contact from './components/contact/Contact'
-import Footer from './components/footer/Footer'
+import Navbar from "./components/navbar/Navbar";
+import Hero from "./components/hero/Hero";
+import About from "./components/about/About";
+import Services from "./components/services/Services";
+import Projects from "./components/projects/Projects";
+import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/Footer";
 
 const App = () => {
-  return (
-    <div>
-      <Navbar />
-      <Hero />
-      <About />
-      <Services />
-      <Projects />
-      <Contact />
-      <Footer />
-    </div>
-  )
-}
+	return (
+		<div>
+			<Navbar />
+			<Hero />
+			<About />
+			<Services />
+			<Projects />
+			<Contact />
+			<Footer />
+		</div>
+	);
+};
 
-export default App
+export default App;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import { useRef, useState } from "react";
 import code from "../../assets/code.png";
 import AnchorLink from "react-anchor-link-smooth-scroll";
 import menu_open from "../../assets/menu_open.svg";
@@ -17,102 +17,62 @@ const Navbar = () => {
 	};
 
 	return (
-		<div className="flex static items-center justify-between sm:sticky -top-4 mx-16 my-6 sm:mx-24 sm:my-2 sm:px-4">
+		<div className="flex sticky items-center justify-between top-2 mx-4 sm:mx-8 md:mx-16 lg:mx-24 xl:mx-32 my-6 sm:my-2 px-4 sm:px-6 md:px-8 lg:px-10">
+			{/* Logo */}
 			<img
 				className="w-12 sm:w-14 rounded-full hover:animate-spin"
 				src={code}
 				alt="logo"
 			/>
+
+			{/* Menu Open Icon (Visible on Small Screens) */}
 			<img
-				className="w-8 block fixed right-12 sm:hidden"
+				className="w-8 block fixed right-6 sm:hidden"
 				src={menu_open}
 				onClick={openMenu}
 				alt="menu-open"
 			/>
 
+			{/* Menu List */}
 			<ul
 				ref={menuRef}
 				onClick={closeMenu}
-				className="w-[350px] h-full sm:w-10/12 fixed sm:static sm:flex flex-col sm:flex-row z-[10] sm:z-0 transition-[right] sm:transition-none duration-500 right-[-350px] items-start justify-start sm:justify-center top-0 gap-6 sm:gap-2 my-2 pt-6 bg-black sm:bg-transparent list-none sm:text-xl"
+				className="w-[350px] h-full sm:w-auto fixed sm:static sm:flex flex-col sm:flex-row z-[10] sm:z-0 transition-[right] sm:transition-none duration-500 right-[-350px] sm:right-0 items-start sm:items-center justify-start sm:justify-center top-0 gap-6 sm:gap-4 md:gap-6 my-2 pt-6 sm:pt-0 bg-black sm:bg-transparent list-none text-xl"
 			>
+				{/* Menu Close Icon (Visible on Small Screens) */}
 				<img
-					className="w-5 block sm:hidden relative top-0 left-72"
+					className="w-5 block sm:hidden absolute top-6 left-72"
 					src={menu_close}
 					alt="menu-close"
 				/>
-				<li className="flex-row pl-12 pb-5 gap-5 text-2xl sm:flex sm:flex-col sm:gap-1 cursor-pointer">
-					<AnchorLink
-						className="text-white decoration-none"
-						offset={50}
-						href="#home"
-					>
-						<p onClick={() => setMenu("home")}>Home</p>
-					</AnchorLink>
-					{menu === "home" ? (
-						<div className="w-20 sm:w-full flex border-b border-2 rounded border-white sm:border-primary"></div>
-					) : (
-						<></>
-					)}
-				</li>
-				<li className="flex-row pl-12 pb-5 gap-5 text-2xl sm:flex sm:flex-col sm:gap-1 cursor-pointer">
-					<AnchorLink
-						className="text-white decoration-none"
-						offset={50}
-						href="#about"
-					>
-						<p onClick={() => setMenu("about")}>About Me</p>
-					</AnchorLink>
-					{menu === "about" ? (
-						<div className="w-28 sm:w-full flex border-b border-2 rounded border-white sm:border-primary"></div>
-					) : (
-						<></>
-					)}
-				</li>
-				<li className="flex-row pl-12 pb-5 gap-5 text-2xl sm:flex sm:flex-col sm:gap-1 cursor-pointer">
-					<AnchorLink
-						className="text-white decoration-none"
-						offset={50}
-						href="#services"
-					>
-						<p onClick={() => setMenu("services")}>Services</p>
-					</AnchorLink>
-					{menu === "services" ? (
-						<div className="w-24 sm:w-full flex border-b border-2 rounded border-white sm:border-primary"></div>
-					) : (
-						<></>
-					)}
-				</li>
-				<li className="flex-row pl-12 pb-5 gap-5 text-2xl sm:flex sm:flex-col sm:gap-1 cursor-pointer">
-					<AnchorLink
-						className="text-white decoration-none"
-						offset={50}
-						href="#projects"
-					>
-						<p onClick={() => setMenu("projects")}>My Projects</p>
-					</AnchorLink>
-					{menu === "projects" ? (
-						<div className="w-32 sm:w-full flex border-b border-2 rounded border-white sm:border-primary"></div>
-					) : (
-						<></>
-					)}
-				</li>
-				<li className="flex-row pl-12 pb-5 gap-5 text-2xl sm:flex sm:flex-col sm:gap-1 cursor-pointer">
-					<AnchorLink
-						className="text-white decoration-none"
-						offset={50}
-						href="#contact"
-					>
-						<p onClick={() => setMenu("contact")}>Contact</p>
-					</AnchorLink>
-					{menu === "contact" ? (
-						<div className="w-24 sm:w-full flex border-b border-2 rounded border-white sm:border-primary"></div>
-					) : (
-						<></>
-					)}
-				</li>
+
+				{/* Menu Items */}
+				{["home", "about", "services", "projects", "contact"].map(
+					(item) => (
+						<li
+							key={item}
+							className="flex-row pl-12 md:pl-2 pb-5 sm:pb-0 gap-5 text-2xl sm:text-lg md:text-xl sm:flex sm:flex-col sm:gap-1 cursor-pointer transition ease-in-out delay-10 hover:-translate-y-1 hover:scale-110 duration-300"
+						>
+							<AnchorLink
+								className="text-white decoration-none"
+								offset={50}
+								href={`#${item}`}
+							>
+								<p onClick={() => setMenu(item)}>
+									{item.charAt(0).toUpperCase() +
+										item.slice(1)}
+								</p>
+							</AnchorLink>
+							{menu === item && (
+								<div className="w-20 sm:w-full flex border-b border-2 rounded border-white sm:border-primary"></div>
+							)}
+						</li>
+					)
+				)}
 			</ul>
 
-			<div className="hidden sm:inline px-5 py-3 rounded-3xl bg-primary text-l cursor-pointer transition ease-in-out delay-10 hover:-translate-y-1 hover:scale-110 duration-300">
+			{/* Connect Button (Hidden on Small Screens) */}
+			<div className="hidden sm:inline px-5 py-3 rounded-3xl bg-primary text-lg sm:text-base cursor-pointer transition ease-in-out delay-10 hover:-translate-y-1 hover:scale-110 duration-300">
 				<AnchorLink
 					className="text-white decoration-none"
 					offset={50}
